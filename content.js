5dacbcbbbc19b2cba85146ca8789f64c71abbfc4
Juliana Hong <julianahhong@gmail.com>
2016-10-05 17:01:18 -0400

    Clean up code

--- content.js
+++ content.js
@@ -2,46 +2,14 @@ document.documentElement.style.height = '100%';
 document.body.style.height = '100%';
 document.documentElement.style.width = '100%';
 document.body.style.width = '100%';
-
 var url = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
 
-var div = document.createElement( 'div' );
-var btnForm = document.createElement( 'form' );
-var btn = document.createElement( 'input' );
-
-//append all elements
-document.body.appendChild( div );
-div.appendChild( btnForm );
-btnForm.appendChild( btn );
-//set attributes for div
-// div.id = 'myDivId';
-// div.style.position = 'fixed';
-// div.style.top = '50%';
-// div.style.left = '50%';
-// div.style.width = '100%';   
-// div.style.height = '100%';
-// div.style.backgroundColor = 'red';
-
-//set attributes for btnForm
-btnForm.action = '';
-
-//set attributes for btn
-//"btn.removeAttribute( 'style' );
-btn.type = 'button';
-btn.value = 'hello';
-btn.style.position = 'absolute';
-btn.style.top = '50%';
-btn.style.left = '50%';
-
 $(document).ready(function(){
-  $("body").append("<div id='hover-box' style='background-color: yellow; position:absolute; width:100px; height:100px'></div>");
-
-  $("body").append("<div id='translation-box' style='background-color: lightblue; position:absolute; width:200px; height:100px'></div>");
-
   $(document).on("mouseover", ".userContent", function(){
+    $("body").append("<div id='hover-box' style='background-color: yellow; position:absolute; width:100px; height:100px'></div>");
+    $("body").append("<div id='translation-box' style='background-color: lightblue; position:absolute; width:200px; height:100px'></div>");
     var position = $(this).offset();
     var width = $(this).width();
-    console.log(position);
 
     $("#hover-box").css("left", position.left + "px");
     $("#hover-box").css("top", position.top + "px");
@@ -53,19 +21,9 @@ $(document).ready(function(){
     $("#translation-box").css("height", $(this).height() + "px");
     $("#translation-box").css("z-index:10000000");
 
-    console.log($(this).html());
     var content = $(this).find("p").first().html();
-    console.log(content);
-    $.get(url + content, function(data){
-      console.log(data);
+    $.get(url + content, function(data) {
       $("#translation-box").html(data.data.translations[0].translatedText);
-
-      // $.post(watsonurl, { html: content , apikey: myapikey, extract: "doc-emotion" }, function(data){
-        // do some stuff iwth result
-      // })
     });
-    // $("#hover-box").css("left", position.left + "px");
-    // $("#hover-box").css("top", position.top + "px");
   });
 });
-// css("background-color", "blue");
