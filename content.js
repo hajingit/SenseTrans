f40869cad37668c0ff8b009ad8a3939b1ee08fe4
Juliana Hong <julianahhong@gmail.com>
2016-11-02 17:58:42 -0400

    Move urls to top

--- content.js
+++ content.js
@@ -2,7 +2,9 @@ document.documentElement.style.height = '100%';
 document.body.style.height = '100%';
 document.documentElement.style.width = '100%';
 document.body.style.width = '100%';
-var url = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
+var watsonUrl = "https://access.alchemyapi.com/calls/html/HTMLGetCombinedData";
+var alchemyApiKey = "07cb048088ebb9dc710d357c380108f66e3fa29a";
+var translateUrl = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
 
 $(document).ready(function(){
   $(document).on("mouseover", ".userContent", function(){
@@ -23,14 +25,13 @@ $(document).ready(function(){
     $("#translation-box").css("padding", "20px");
 
     var content = $(this).find("p").first().html();
-    $.get(url + content, function(data) {
+    $.get(translateUrl + content, function(data) {
       $("#translation-box").html("<h2> Translation: </h2>" + data.data.translations[0].translatedText);
     });
 
     var words = $(this).html();
     //console.log(words);
-    var watsonUrl = "https://access.alchemyapi.com/calls/html/HTMLGetCombinedData";
-    var alchemyApiKey = "07cb048088ebb9dc710d357c380108f66e3fa29a";
+
     $.post(watsonUrl, {html: words, apikey: alchemyApiKey, outputMode: "json", extract: "doc-emotion"},function(data){
       //console.log(data);
       var count = Object.keys(data).length;
