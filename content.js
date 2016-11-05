147723bc0ead248d1c7c1fd99dc9360b9e50ddd3
Juliana Hong <julianahhong@gmail.com>
2016-11-05 14:24:17 -0400

    Move urls

--- content.js
+++ content.js
@@ -2,7 +2,9 @@ document.documentElement.style.height = '100%';
 document.body.style.height = '100%';
 document.documentElement.style.width = '100%';
 document.body.style.width = '100%';
-var url = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
+var translateUrl = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
+var watsonUrl = "https://access.alchemyapi.com/calls/html/HTMLGetCombinedData";
+var alchemyApiKey = "07cb048088ebb9dc710d357c380108f66e3fa29a";
 
 $(document).ready(function(){
   $(document).on("mouseover", ".userContent", function(){
@@ -29,8 +31,7 @@ $(document).ready(function(){
 
     var words = $(this).html();
     //console.log(words);
-    var watsonUrl = "https://access.alchemyapi.com/calls/html/HTMLGetCombinedData";
-    var alchemyApiKey = "07cb048088ebb9dc710d357c380108f66e3fa29a";
+
     $.post(watsonUrl, {html: words, apikey: alchemyApiKey, outputMode: "json", extract: "doc-emotion"},function(data){
       //console.log(data);
       var count = Object.keys(data).length;
