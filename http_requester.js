fd9d86ae514aeaf2b2b4f6589ed5977fae5a8bc3
Trevor Edwards <tre23@cornell.edu>
2017-03-05 17:37:19 -0500

    payload background

--- http_requester.js
+++ http_requester.js
@@ -1,8 +1,7 @@
 var loggingUrl = "http://foreign-language-chrome-ext.herokuapp.com/log";
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        $.post(loggingUrl, request, function (response) {
-            console.log('resp. receipved, sending');
+        $.post(loggingUrl, {"payload": JSON.stringify(request)}, function (response) {
             sendResponse(response);
         });
 
