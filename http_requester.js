9527707117873a6208e71f5b018cf3a4945d03f1
jwg245 <jwg245@cornell.edu>
2017-07-31 13:39:59 -0400

    Delete http_requester.js

--- http_requester.js
+++
@@ -1,9 +0,0 @@
-var loggingUrl = "http://foreign-language-chrome-ext.herokuapp.com/log";
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        $.post(loggingUrl, {"payload": JSON.stringify(request)}, function (response) {
-            sendResponse(response);
-        });
-
-        return true;
-    });
