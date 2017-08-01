994bf745d7f19bbafc982687cde51bbf5f64a9a3
kms435 <kms435@cornell.edu>
2017-08-01 12:20:50 -0400

    Delete http_requester.js

--- http_requester.js
+++
@@ -1,8 +0,0 @@
-var loggingUrl = "http://integrated-slide-logger.herokuapp.com/log";
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        $.post(loggingUrl, {"payload": JSON.stringify(request)}, function (response) {
-            sendResponse(response);
-        });
-        return true;
-    });
